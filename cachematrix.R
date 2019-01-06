74170e239331f0ffad0931e81388d6c865d58cbf
Andres Sanchez <43328806+asanchezpadron@users.noreply.github.com>
2019-01-06 17:03:48 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -7,17 +7,22 @@ makeCacheMatrix <- function(x = matrix()) {
 
       ## Set inverso as NULL.
       inverso <- NULL
+      
             ## This sub_variable is to assign the value of the matrix to x.
             set <- function(a) {
                   x <<- a
                   inverso <- NULL
             }
+      
             ## This sub_variable is to retrive the value of x.
             get <- function() x
+      
             ## This sub_variable is to assign the matrix inversion of x to "inverso".
             set_inverso <- function() inverso <<- solve(x)
+      
             ## This sub_variable is to retrive the value of "inverso".
             get_inverso <- function() inverso
+      
             ## This is the list of sub_variables of "makeCacheMatrix".
             list(set = set, get = get, set_inverso = set_inverso, 
                  get_inverso = get_inverso)
@@ -28,12 +33,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## (and the matrix has not changed), then the cachesolve should retrieve 
 ## the inverse from the cache.
 cacheSolve <- function(x, ...) {
+      
       ## If inverso is not NULL then bring the value of inverso from the cache.
       inverso <- x$get_inverso()
             if(!is.null(inverso)){
                   message("Getting cached data.")
                   return(inverso)
             }
+      
       ## If inverso value was NULL then recalculate the matrix inverse value.
       matriz <- x$get()
       inverso <- solve(matriz, ...)
